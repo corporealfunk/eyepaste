1f21453e748169a0cd71408e9d229cfbe1421608
Jon Moniaci <jonmoniaci@gmail.com>
2012-02-06 00:55:40 -0800

    exiting intake script gracefully after exception logging

--- scripts/intake.rb
+++ scripts/intake.rb
@@ -17,10 +17,16 @@ begin
   email = Eyepaste::Email.parse_raw_email(content)
 rescue ArgumentError => e
   LOGGER.warn "#{e.class}: #{e.message}:\n#{content}"
+
+  # exit gracefully
+  exit 0
 end
 
 begin
   storage.append_email(email.to, email)
 rescue NoMethodError, Encoding::UndefinedConversionError => e
   LOGGER.warn "#{e.class}: #{e.message}:\n#{content}"
+
+  # exit gracefully
+  exit 0
 end
